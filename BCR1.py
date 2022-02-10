8b513b2043741a9608531076d1e3fb68045717c8
Bonilapa <bonilapka@gmail.com>
2022-02-10 19:17:04 +0300

    Update BCR1.py

--- BCR1.py
+++ BCR1.py
@@ -22,3 +22,28 @@ for i_episode in range(10):
 
             break
 env.close()
+
+'''
+pendulum.py
+
+1. What is encoded in observation vector?
+
+2D Euclidean space coordinates and angular velocity, and their limits
+
+2. What is encoded in action vector?
+
+Pendulum torque and its limits
+
+3. how the initial conditions are varied in env.reset?
+
+starting angle is [-pi, pi] and starting angular velocity in [-1, 1]
+
+4. How the reward is calculated?
+
+-(theta^2 + 0.1*theta_dt^2 + 0.001*torque^2)
+
+5. When the episode terminated?
+
+After 200 steps, no other criteria
+
+'''
